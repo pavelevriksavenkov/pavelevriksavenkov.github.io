3c8a1086cc5473d8d465c9ecfcf1058f6ea63aec
pavelevriksavenkov <savenkovp85@gmail.com>
2020-03-02 00:20:25 +0500

    change style for adaptive

--- montazservis/js/main.js
+++ montazservis/js/main.js
@@ -1,6 +1,9 @@
 var mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     loop: true,
+    // autoplay: {
+    //     delay: 3000,
+    // },
     navigation: {
         nextEl: '.button-next',
         prevEl: '.button-prev',
